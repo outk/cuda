4b23b365b8f8f0b0e5fbe2de83bfd44d280f0b4e
QI <example@gmail.com>
2020-01-22 19:06:42 +0900

    downloaded sampl;e programs

---
+++ sumArraysOnGPU-timer.cu
@@ -0,0 +1,88 @@
+#include <cuda_runtime.h>
+#include <stdio.h>
+#include <sys/time.h>
+
+#define CHECK(call) {                                                       \
+    cosnt cudaError_t error = call;                                         \
+    if (error != cudaSuccess) {                                             \
+        printf("Error: %s:%d, ", __FILE__, __LINE__);                       \
+        printf("code:%d, reason: %s\n", error, cudaGetErrorString(error));  \
+        exit(1);                                                            \
+    }                                                                       \
+}     
+
+double cpuSecond() {
+    struct timeval tp;
+    gettimeofday(&tp, NULL);
+    return ((double)tp.tv_sec + (double)tp.tv_usec * 1.e-6);
+}
+
+int main(int argc, char **argv) {
+    printf("%s Starting...\n", argv[0]);
+
+    int dev = 0;
+    cudaDeviceProp deviceProp;
+    CHECK(cudaGetDeviceProperties(&deviceProp, dev));
+    printf("Using Device %d: %s\n", dev, deviceProp.name);
+    CHECK(cudaSetDevice(dev));
+
+    int nElem = 1 << 24;
+    printf("Vector size %d\n", nElem);
+
+    size_t nBytes = nElem * sizeof(float);
+
+    float *h_A, *h_B, *hostRef, *gpuRef;
+    h_A = (float *)malloc(nBytes);
+    h_B = (float *)malloc(nBytes);
+    hostRef = (float *)malloc(nBytes);
+    gpuRef = (float *)malloc(nBytes);
+    
+    double iStart, iElaps;
+
+    iStart = cpuSecond();
+    initialData (h_A, nElem);
+    initialData (h_B, nElem);
+    iElaps = cpuSecond() - iStart;
+    memset(hostRef, 0, nBytes);
+    memset(gpuRef, 0, nBytes);
+
+    iStart = cpuSecond();
+    sumArraysOnHost (h_A, h_B, hostRef, nElem);
+    iElaps = cpuSecond() - iStart;
+
+    float *d_A, *d_B, *d_C;
+    CHECK(cudaMalloc((float **)&d_A, nBytes));
+    CHECK(cudaMalloc((float **)&d_B, nBytes));
+    CHECK(cudaMalloc((float **)&d_C, nBytes));
+    
+    CHECK(cudaMemcpy(d_A, h_A, nBytes, cudaMemcpyHostToDevice));
+    CHECK(cudaMemcpy(d_B, h_B, nBytes, cudaMemcpyHostToDevice));
+    CHECK(cudaMemcpy(d_C, gpuRef, nBytes, cudaMemcpyHostToDevice));
+    
+    int iLen = 1024;
+    dim3 block(iLen);
+    dim3 grid((nElem + block.x - 1) / block.x);
+
+    iStart = cpuSecond();
+    sumArraysOnGPU <<<grid, block>>>(d_A, d_B, d_C, nElem);
+    CHECK(cudaDeviceSynchronize());
+    iElaps = cpuSecond() - iStart;
+    printf("sumArraysOnGPU <<<%d, %d>>> Time elapsed %f sec\n", grid.x, block.x, iElaps);
+
+    CHECK(cudaGetLastError());
+
+    CHECK(cudaMemcpy(gpuRef, d_C, nBytes, cudaMemcpyDeviceToHost));
+
+    checkResult(hostRef, gpuRef, nElem);
+
+    CHECK(cudaFree(d_A));
+    CHECK(cudaFree(d_B));
+    CHECK(cudaFree(d_C));
+    
+    free(h_A);
+    free(h_B);
+    free(hostRef);
+    free(gpuRef);
+    
+    return(0);
+}
